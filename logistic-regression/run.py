1217303b5d4d878e22f0695b019ec7f88fc32eed
Botond Cseke <csekeboti@gmail.com>
2022-11-28 13:43:14 +0100

    implementation of the diagonal approximation

--- logistic-regression/run.py
+++ logistic-regression/run.py
@@ -4,7 +4,7 @@ import yaml
 
 import pytorch_lightning as pl
 from pytorch_lightning import Trainer
-from model_logreg_mvn import ModelLogisicRegressionMvn
+from model_logreg_dn import ModelLogisicRegressionMvn
 from dataset_npz import DataModuleFromNPZ
 
 
@@ -14,7 +14,7 @@ def main():
     dm = DataModuleFromNPZ(
         data_dir="data_logistic_regression_2d",
         feature_labels=["inputs", "targets"],
-        batch_size=-1,
+        batch_size=256,
         num_workers=4,
         shuffle_training=False
     )
@@ -25,17 +25,42 @@ def main():
     dm.prepare_data()
     dm.setup(stage="fit")
 
-    model   = ModelLogisicRegressionMvn(
+    #
+    # training a multivariate model
+    #
+    model_mvn = ModelLogisicRegressionMvn(
                 2,
                 dm.size_train(),
+                is_diagonal=False,
                 scale_prior=10.0,
                 optimizer_name="RMSprop", 
-                optimizer_lr=0.1)
+                optimizer_lr=0.1,
+                save_path="runs/models/multivariate")
+
+
+    trainer = Trainer(max_epochs=200)
     
-    trainer = Trainer(max_epochs=50)
+    trainer.fit(model_mvn, dm)
+    trainer.test(model_mvn, dm)
     
-    trainer.fit(model, dm)
-    trainer.test(model, dm)
+    #
+    # training a diagonal model
+    #
+    model_diag = ModelLogisicRegressionMvn(
+                2,
+                dm.size_train(),
+                is_diagonal=True,
+                scale_prior=10.0,
+                optimizer_name="RMSprop", 
+                optimizer_lr=0.1,
+                save_path="runs/models/diagonal")
+
+
+    trainer = Trainer(max_epochs=200)
+    
+    trainer.fit(model_diag, dm)
+    trainer.test(model_diag, dm)
+
 
 if __name__ == "__main__":
     main()
